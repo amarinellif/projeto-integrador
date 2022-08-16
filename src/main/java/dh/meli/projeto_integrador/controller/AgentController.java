858dd20eba8c3fd127cd05b32d3ab9627c3c4895
amarinellif <amanda.marinelli@mercadolivre.com>
2022-08-16 19:31:19 -0300

    feat: add metodo de busca de produtos armazenados em sections erradas atraves da temperatura

--- src/main/java/dh/meli/projeto_integrador/controller/AgentController.java
+++ src/main/java/dh/meli/projeto_integrador/controller/AgentController.java
@@ -2,7 +2,10 @@ package dh.meli.projeto_integrador.controller;
 
 import dh.meli.projeto_integrador.dto.dtoOutput.WarningDueDateDto;
 import dh.meli.projeto_integrador.dto.dtoOutput.WarningTempDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.WrongPlaceBatchDto;
 import dh.meli.projeto_integrador.dto.dtoOutput.WrongTempDto;
+import dh.meli.projeto_integrador.model.IWrongPlaceBatch;
+import dh.meli.projeto_integrador.repository.IBatchRepository;
 import dh.meli.projeto_integrador.service.WarehouseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +24,9 @@ public class AgentController {
     @Autowired
     WarehouseService warehouseService;
 
+    @Autowired
+    IBatchRepository repo;
+
     @GetMapping("fresh-products/agent/warning-temp-batches/{id}")
     public ResponseEntity<WarningTempDto> getWarningTempBatches(@PathVariable long id){
         return ResponseEntity.ok(warehouseService.getWarningTempBatchesByWarehouse(id));
@@ -28,17 +34,16 @@ public class AgentController {
 
     @GetMapping("fresh-products/agent/warning-duedate-batches/{id}")
     public ResponseEntity<WarningDueDateDto> getProductBatches(@PathVariable long id, @RequestParam int daysUntil){
-        return ResponseEntity.ok(warehouseService.getWarningDueDateBatchesByWarehouse(daysUntil,id));
+        return ResponseEntity.ok(warehouseService.getWarningDueDateBatchesByWarehouse(id,daysUntil));
     }
-
-    //@GetMapping("fresh-products/agent/warning-duedate-batches/{id}")
-    //public ResponseEntity<WarningDueDateDto> getWrongPlaceBaches(@PathVariable long id, @RequestParam int daysUntil){
-    //    return ResponseEntity.ok(warehouseService.getWarningDueDateBatchesByWarehouse(daysUntil,id));
-    //}
 
     @GetMapping("fresh-products/agent/warning-temp-section/{id}")
-    public ResponseEntity<WrongTempDto> getWrongPlaceBaches(@PathVariable long id){
-        return ResponseEntity.ok(warehouseService.getWrongTempSection(id));
+    public ResponseEntity<WrongTempDto> getWrongTempBatches(@PathVariable long id){
+       return ResponseEntity.ok(warehouseService.getWrongTempSection(id));
     }
 
+    @GetMapping("fresh-products/agent/wrong-place-batches/{id}")
+    public ResponseEntity<WrongPlaceBatchDto> getWrongPlaceBatches(@PathVariable long id){
+        return ResponseEntity.ok(warehouseService.getWrongPlaceBatches(id));
+    }
 }
