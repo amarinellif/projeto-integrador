1b00133dc8bf42b03c36393a4df1a7cfbf27ca42
amarinellif <amanda.marinelli@mercadolivre.com>
2022-08-16 09:43:55 -0300

    feat: adiciona novos metodos de busca de possíveis problemas com temperatura e data de validade no warehouse

--- src/main/java/dh/meli/projeto_integrador/service/IWarehouseService.java
+++ src/main/java/dh/meli/projeto_integrador/service/IWarehouseService.java
@@ -1,17 +1,46 @@
 package dh.meli.projeto_integrador.service;
 
+import dh.meli.projeto_integrador.dto.dtoOutput.WarningDueDateDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.WarningTempDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.WrongTempDto;
 import dh.meli.projeto_integrador.model.Warehouse;
 
 /**
  * Interface to specify service methods implemented on WarehouseService class.
- * @author Diovana Valim, Thiago Guimaraes;
+ *
+ * @author Diovana Valim, Thiago Guimaraes and Amanda Marinelli
  * @version 0.0.1
  */
 public interface IWarehouseService {
     /**
      * Method for to find Warehouse by Id
+     *
      * @param id long
      * @return an object of type Warehouse
      */
     Warehouse findWarehouse(long id);
+
+    /**
+     * Method for to possibles Warnings about incorrects temperatures by Warehouse Id
+     *
+     * @param id long
+     * @return an object of type WarningTempDto
+     */
+    WarningTempDto getWarningTempBatchesByWarehouse(long id);
+
+    /**
+     * Method for to possibles Warnings about incorrects temperatures by Warehouse Id
+     *
+     * @param id long
+     * @return an object of type WarningDueDateDto
+     */
+    WarningDueDateDto getWarningDueDateBatchesByWarehouse(int daysUntil, long id);
+    /**
+     * Method for to possibles Warnings about incorrects temperatures by Warehouse Id
+     *
+     * @param id long
+     * @return an object of type WrongTempDto
+     */
+    WrongTempDto getWrongTempSection(Long warehouseId);
+
 }
--- src/main/java/dh/meli/projeto_integrador/service/WarehouseService.java
+++ src/main/java/dh/meli/projeto_integrador/service/WarehouseService.java
@@ -1,15 +1,22 @@
 package dh.meli.projeto_integrador.service;
 
+import dh.meli.projeto_integrador.dto.dtoOutput.WarningDueDateDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.WarningTempDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.WrongTempDto;
 import dh.meli.projeto_integrador.exception.ResourceNotFoundException;
+import dh.meli.projeto_integrador.model.Batch;
 import dh.meli.projeto_integrador.model.Warehouse;
+import dh.meli.projeto_integrador.repository.IBatchRepository;
 import dh.meli.projeto_integrador.repository.IWarehouseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
  * Class responsible for business rules and communication with the Warehouse Repository layer;
+ *
  * @author Diovana Valim;
  * @version 0.0.1
  */
@@ -22,8 +29,12 @@ public class WarehouseService implements IWarehouseService {
     @Autowired
     private IWarehouseRepository warehouseRepository;
 
+    @Autowired
+    private IBatchRepository batchRepository;
+
     /**
      * Method to find a warehouse by id;
+     *
      * @param id of type long. Warehouse identifier;
      * @return an object of type Warehouse;
      */
@@ -36,5 +47,51 @@ public class WarehouseService implements IWarehouseService {
         }
 
         return warehouse.get();
+    }
+
+    /**
+     * Method to find warnings about high temperatature on batches by id Warehouse;
+     *
+     * @param id of type long. Warehouse identifier;
+     * @return an object of type WarningTempDto;
+     */
+    @Override
+    public WarningTempDto getWarningTempBatchesByWarehouse(long id) {
+        Warehouse warehouse = findWarehouse(id);
+        List<Batch> batchList = batchRepository.getWarningTemperaturesByWarehouseId(id);
+
+        if (batchList.isEmpty()) {
+            throw new ResourceNotFoundException("Everything is okay with this Warehouse. No batch warnings found.");
+        }
+        return new WarningTempDto(warehouse, batchList);
+    }
+
+    /**
+     * Method to find warnings about high temperatature on batches by id Warehouse;
+     *
+     * @param id of type long. Warehouse identifier;
+     * @return an object of type WarningTempDto;
+     */
+    @Override
+    public WarningDueDateDto getWarningDueDateBatchesByWarehouse(int daysUntil,long id) {
+        Warehouse warehouse = findWarehouse(id);
+        List<Batch> batchList = batchRepository.getWarningDueDateByWarehouseId(daysUntil,id);
+
+        if (batchList.isEmpty()) {
+            throw new ResourceNotFoundException("Everything is okay with this Warehouse. No batch warnings found.");
+        }
+        return new WarningDueDateDto(warehouse,batchList);
+    }
+
+    @Override
+    public WrongTempDto getWrongTempSection(Long id){
+        //TODO DESVINCULAR A QUERY DO BATCH, atrelar a sessão :)
+        Warehouse warehouse = findWarehouse(id);
+        List<Batch> batchList = batchRepository.getWrongTempSection(id);
+
+        if (batchList.isEmpty()) {
+            throw new ResourceNotFoundException("Everything is okay with this Warehouse. No batch warnings found.");
+        }
+        return new WrongTempDto(warehouse, batchList);
     }
 }
