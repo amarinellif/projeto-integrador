50658e3e78aa30e27662c2aa7f51e0ae8b5431ac
amarinellif <amanda.marinelli@mercadolivre.com>
2022-08-16 10:12:58 -0300

    feat: adiciona os endpoints para serem acessados pelo agent com informações sobre o warehouse

---
+++ src/main/java/dh/meli/projeto_integrador/controller/AgentController.java
@@ -0,0 +1,44 @@
+package dh.meli.projeto_integrador.controller;
+
+import dh.meli.projeto_integrador.dto.dtoOutput.WarningDueDateDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.WarningTempDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.WrongTempDto;
+import dh.meli.projeto_integrador.service.WarehouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Class responsible for processing user's requests and generating appropriated HTTP responses;
+ * @author Amanda Marinelli
+ * @version 0.0.1;
+ */
+@RestController
+@RequestMapping("/api/v1")
+public class AgentController {
+
+
+    @Autowired
+    WarehouseService warehouseService;
+
+    @GetMapping("fresh-products/agent/warning-temp-batches/{id}")
+    public ResponseEntity<WarningTempDto> getWarningTempBatches(@PathVariable long id){
+        return ResponseEntity.ok(warehouseService.getWarningTempBatchesByWarehouse(id));
+    }
+
+    @GetMapping("fresh-products/agent/warning-duedate-batches/{id}")
+    public ResponseEntity<WarningDueDateDto> getProductBatches(@PathVariable long id, @RequestParam int daysUntil){
+        return ResponseEntity.ok(warehouseService.getWarningDueDateBatchesByWarehouse(daysUntil,id));
+    }
+
+    //@GetMapping("fresh-products/agent/warning-duedate-batches/{id}")
+    //public ResponseEntity<WarningDueDateDto> getWrongPlaceBaches(@PathVariable long id, @RequestParam int daysUntil){
+    //    return ResponseEntity.ok(warehouseService.getWarningDueDateBatchesByWarehouse(daysUntil,id));
+    //}
+
+    @GetMapping("fresh-products/agent/warning-temp-section/{id}")
+    public ResponseEntity<WrongTempDto> getWrongPlaceBaches(@PathVariable long id){
+        return ResponseEntity.ok(warehouseService.getWrongTempSection(id));
+    }
+
+}
